48c2d21e7e745f8b59b44b665a5b4eb3ae7b2bee
k2sebeom <moses97@gmail.com>
2022-04-22 18:03:02 +0900

    feat: Change to polygon main net

--- src/constants/chain.js
+++ src/constants/chain.js
@@ -1,6 +1,6 @@
 
 
-export const CONTRACT_ADDR = "0xB27b37888a83BFd48b3b3e7B9469F17219CdD67F";
+export const CONTRACT_ADDR = "0x83Fd3aF8b4FcA053D7b0BC73cAcfE17c25e05b08";
 export const CONTRACT_ABI = [
     "function nakseo(uint256 _tokenId, uint256 _msgId) view returns (address, string memory)",
     "function nakseoCount(uint256 _tokenId) view returns (uint256)",
@@ -12,10 +12,10 @@ export const CONTRACT_ABI = [
     "function tokenURI(uint256 _tokenId) view public returns(string memory)"
 ];
 
-export const PROVIDER_URL = "https://rpc-mumbai.maticvigil.com";
+export const PROVIDER_URL = "https://polygon-rpc.com/";
 
 export const CHAIN_CONFIG = {
-    chainId: '0x13881',
-    chainName: 'Polygon Mumbai Testnet',
+    chainId: '0x89',
+    chainName: 'Polygon Mainnet',
     rpcUrls: [PROVIDER_URL]
 }
